503477a315cf7fdd371b28ca4c03a9a128347a66
Cabrera-git <a01025453@itesm.mx>
2022-05-18 13:03:04 -0500

    Update Act3_4.exs

--- Elixir/Act3_4/Act3_4.exs
+++ Elixir/Act3_4/Act3_4.exs
@@ -6,27 +6,29 @@
 
 defmodule RegexParser do
     def case(line) do
-        Regex.run(~r/\s*("\w+")\s*(:)\s*("(\w*[.:]*\s?)+"),?/, line)
-        Regex.run(~r/\s*("\w+")\s*(:)\s*\d+(\.\d+E[\+-]?\d+)?,?/, line)
-        Regex.run(~r/\s*("\w+")\s*(:)\s*(true|false|null),?/, line)
-        Regex.run(~r/[\[\]{}]/, line)
-        Regex.run(~r//, line)
+        case do
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)("(\w*[.:]*\s?)+")/, line) -> # ( object-key)( : )(string )
+
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)(\d+(\.\d+E[\+-]?\d+)?)/, line) -> # ( object-key)( : )(number )
+
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)((true|false|null)\s*)/, line) -> # ( object-key)( : )(reserved word )
+
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)({\s*)$/, line) -> # ( object-key)( : )({ )[end-of-line]
+
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)(\[\s*)$/, line) -> # ( object-key)( : )([ )[end-of-line]
+
+            Regex.match?(~r/^\s*[,:}\]]+\s*$/, line) -> # [start-of-line] punctuation [end-of-line]
+
+            Regex.match?(~r/(\s*{)(\s*"\w+")(\s*:\s*)("\w+")(\s*,\s*)("\w+")(\s*:\s*)("\w+\(\)"\s*)(}\s*)/, line) -> # ( [)( string)( : )(string)( , )(string)( : )(string() )(] )
+
+            Regex.match?(~r/(\s*"\w+")(\s*:\s*)("\w+\.\w+\s*=\s*\(\w+\.\w+\s*\/\s*\d+\)\s*\*\s*\d+;")\s*/, line) -> # ( object-key)( : )(string )
+
+        end
     end
 
-    def color(token, id) do
+    def color(item, type) do
         case do
-            id == 's' ->
-                html_add = '<p id="string">'+token+'<\p>'
-            id == 'n' ->
-                html_add = '<p id="number">'+token+'<\p>'
-            id == 'k' ->
-                html_add = '<p id="keyword">'+token+'<\p>'
-            id == 'g' ->
-                html_add = '<p id="key">'+token+'<\p>'
-            id == 'p' ->
-                html_add = '<p id="separator">'+token+'<\p>'
-        end
 
-        html_add
+        end
     end
 end
