604b25f2cec7e4cd291f9eb3f66ed58d14d9e3f4
Cabrera-git <a01025453@itesm.mx>
2022-06-09 13:05:39 -0500

    Update Act5_2.exs
    + 5.2 fully works

--- Elixir/Act5_2/Act5_2.exs
+++ Elixir/Act5_2/Act5_2.exs
@@ -5,6 +5,14 @@
 # 2022-05-20
 
 defmodule Hw.Primes do
+
+    def timer(function) do
+        function
+        |> :timer.tc()
+        |> elem(0)
+        |> Kernel./(1_000_000)
+  end
+
     def sum_primes(lim, current \\ 2, sum \\ 0) do
         cond do
             current >= lim ->
@@ -21,28 +29,14 @@ defmodule Hw.Primes do
     end
 
     def sum_primes_parallel(lim, thr) do
-        bin_size = Kernel.div(lim,thr)
+        primeLst = primeBin(lim)
+        primeSize = Kernel.length(primeLst)
 
-        1..thr
-        |> Enum.map(&Task.async(fn -> parallel_sum(bin_size) end))
+        primeLst
+        |> Enum.chunk_every(Kernel.div(primeSize,thr))
+        |> Enum.map(&Task.async(fn -> Enum.sum(&1) end))
         |> Enum.map(&Task.await(&1))
         |> Enum.sum()
-        |> IO.inspect()
-    end
-
-    def parallel_sum(lower, upper, sum \\ 0) do
-        cond do
-            lower >= upper ->
-                if prime?(lower) do
-                    sum+lower
-                else
-                    sum
-                end
-            prime?(lower) ->
-                parallel_sum(lower+1,upper,sum+lower)
-            !prime?(lower) -> 
-                parallel_sum(lower+1,upper,sum)
-        end
     end
 
     defp prime?(n, i \\ 2) do
@@ -59,7 +53,22 @@ defmodule Hw.Primes do
                 prime?(n,i+1)
         end
     end
+
+    defp primeBin(finish, current \\ 1, lst \\ []) do
+        cond do
+            current >= finish ->
+                lst
+            prime?(current) ->
+                primeBin(finish,current+1,[current | lst])
+            !prime?(current) ->
+                primeBin(finish,current+1,lst)
+        end        
+    end
 end
 
-# IO.inspect(Hw.Primes.sum_primes(5000000))
-IO.inspect(Hw.Primes.parallel_sum(5000000,8))
+IO.puts("Convencional")
+IO.puts(Hw.Primes.sum_primes(5000000))
+IO.puts(Hw.Primes.timer(fn -> Hw.Primes.sum_primes(5000000) end))
+IO.puts("\nParalelo")
+IO.puts(Hw.Primes.sum_primes_parallel(5000000,8))
+IO.puts(Hw.Primes.timer(fn -> Hw.Primes.sum_primes_parallel(5000000,8) end))
